9c09ee645e81ef040c333ba82d0cb324bd2804b0
Alessandre Livramento <aleliv74@gmail.com>
2019-09-20 00:14:48 -0300

    Implementação _layouts; Routes; global styles

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch } from 'react-router-dom'
+import Route from './Route'
 
 import SignIn from '../pages/SignIn'
 import SignUp from '../pages/SignUp'
@@ -14,10 +15,11 @@ export default function Routes() {
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/resgiter" component={SignUp} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/details" component={Details} />
-      <Route path="/newedit" component={NewEdit} />
-      <Route path="/profile" component={Profile} />
+
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/details" component={Details} isPrivate />
+      <Route path="/newedit" component={NewEdit} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   )
 }
