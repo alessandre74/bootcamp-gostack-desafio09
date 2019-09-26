4951a6d0276f5c634a8a55d5274f7e7466121a4d
Alessandre Livramento <aleliv74@gmail.com>
2019-09-26 00:26:38 -0300

    Implementação redux redux-saga auth

---
+++ src/store/modules/auth/reducer.js
@@ -0,0 +1,22 @@
+import produce from 'immer'
+
+const INICIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+}
+
+export default function auth(state = INICIAL_STATE, action) {
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@auth/SIGN_IN_SUCCESS': {
+        draft.token = action.payload.token
+        draft.signed = true
+        draft.loading = false
+        break
+      }
+
+      default:
+    }
+  })
+}
