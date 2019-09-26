82ebacb9f0f2bc3a73b3e9b342e0ec5b7bb3e8ff
Alessandre Livramento <aleliv74@gmail.com>
2019-09-26 01:23:08 -0300

    redux redux-saga user/reduxpersist

---
+++ src/store/modules/user/reducer.js
@@ -0,0 +1,17 @@
+import produce from 'immer'
+
+const INICIAL_STATE = {
+  profile: null,
+}
+
+export default function user(state = INICIAL_STATE, action) {
+  return produce(state, draft => {
+    switch (action.type) {
+      case '@auth/SIGN_IN_SUCCESS': {
+        draft.profile = action.payload.user
+        break
+      }
+      default:
+    }
+  })
+}
