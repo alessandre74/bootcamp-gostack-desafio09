eb99bf735f6658e38f9d3a9df65195f56ec5bdf6
Alessandre Livramento <aleliv74@gmail.com>
2019-10-01 02:40:13 -0300

    Header/requisições autenticadas

---
+++ src/components/Header/styles.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  background-color: rgba(0, 0, 0, 0.3);
+  padding: 0 30px;
+`
+
+export const Content = styled.div`
+  height: 92px;
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  aside {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    button {
+      border: 0;
+      border-radius: 6px;
+      background-color: #d44059;
+      color: #fff;
+      font-size: 22px;
+      font-weight: bold;
+      padding: 14px 26px;
+    }
+  }
+`
+
+export const Profile = styled.div`
+  display: flex;
+
+  div {
+    text-align: right;
+    margin-right: 30px;
+
+    strong {
+      display: block;
+      font-size: 14px;
+      color: #fff;
+    }
+
+    a {
+      display: block;
+      margin-top: 8px;
+      font-size: 14px;
+      font-weight: normal;
+      color: #999;
+    }
+  }
+`
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import api from '~/services/api'
 
 import { Container } from './styles'
 
 export default function Dashboard() {
+  api.get('meetupUserLogged')
   return (
     <Container>
       <h1>Dashboard</h1>
