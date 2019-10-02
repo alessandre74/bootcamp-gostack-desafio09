2b01943239844fec3aac19622c7ede257a2cf8e3
Alessandre Livramento <aleliv74@gmail.com>
2019-10-01 23:20:21 -0300

    Pagina Dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,13 +1,62 @@
 import React from 'react'
+import { MdAddCircleOutline, MdChevronRight } from 'react-icons/md'
 import api from '~/services/api'
 
-import { Container } from './styles'
+import { Container, Content, MeetupItens } from './styles'
 
 export default function Dashboard() {
   api.get('meetupUserLogged')
   return (
     <Container>
-      <h1>Dashboard</h1>
+      <Content>
+        <header>
+          <strong>Meus meetups</strong>
+          <button type="button">
+            <MdAddCircleOutline size={24} color="#fff" />
+            <strong>Novo meetup</strong>
+          </button>
+        </header>
+
+        <ul>
+          <MeetupItens>
+            <strong>Meus Meetups</strong>
+            <div>
+              <span>24 de junho, ás 20h</span>
+              <button type="button">
+                <MdChevronRight size={28} color="#fff" />
+              </button>
+            </div>
+          </MeetupItens>
+
+          <MeetupItens>
+            <strong>Meus Meetups</strong>
+            <div>
+              <span>24 de junho, ás 20h</span>
+              <button type="button">
+                <MdChevronRight size={28} color="#fff" />
+              </button>
+            </div>
+          </MeetupItens>
+          <MeetupItens>
+            <strong>Meus Meetups</strong>
+            <div>
+              <span>24 de junho, ás 20h</span>
+              <button type="button">
+                <MdChevronRight size={28} color="#fff" />
+              </button>
+            </div>
+          </MeetupItens>
+          <MeetupItens>
+            <strong>Meus Meetups</strong>
+            <div>
+              <span>24 de junho, ás 20h</span>
+              <button type="button">
+                <MdChevronRight size={28} color="#fff" />
+              </button>
+            </div>
+          </MeetupItens>
+        </ul>
+      </Content>
     </Container>
   )
 }
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,3 +1,72 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  margin-top: 60px;
+`
+export const Content = styled.div`
+  height: 38px;
+  max-width: 900px;
+  margin: 0 auto;
+
+  header {
+    max-width: 900px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    strong {
+      text-align: right;
+      color: #fff;
+      font-size: 32px;
+    }
+    button {
+      display: flex;
+      justify-content: space-between;
+      border: 0;
+      border-radius: 6px;
+      background-color: #f94d6a;
+      color: #fff;
+      font-weight: bold;
+      padding: 14px 26px;
+
+      strong {
+        font-size: 22px;
+        margin-left: 10px;
+      }
+    }
+  }
+
+  ul {
+    margin-top: 40px;
+  }
+`
+
+export const MeetupItens = styled.li`
+  background-color: rgba(0, 0, 0, 0.2);
+  margin-top: 20px;
+  font-size: 20px;
+  height: 62px;
+  padding: 14px 26px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  strong {
+    color: #fff;
+  }
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    span {
+      color: #999;
+    }
+
+    button {
+      border: 0;
+      margin-left: 40px;
+      background: none;
+    }
+  }
+`
