a305ba92f33238adb18d66430ee16f78abd0a977
Alessandre Livramento <aleliv74@gmail.com>
2019-10-09 03:26:58 -0300

    implementação layout profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,11 +1,36 @@
 import React from 'react'
-
-import { Container } from './sytles'
+import { Form, Input } from '@rocketseat/unform'
+import { MdAddCircleOutline } from 'react-icons/md'
+import { Container, Content } from './sytles'
 
 export default function Profile() {
+  function handleSubmit(data) {
+    console.tron.log(data)
+  }
   return (
     <Container>
-      <h1>Profile</h1>
+      <Content>
+        <Form onSubmit={handleSubmit}>
+          <Input name="nome" placeholder="Nome Completo" />
+          <Input name="email" type="emai" placeholder="Seu e-mail" />
+
+          <hr />
+
+          <Input name="oldPassword" type="password" placeholder="Senha atual" />
+          <Input name="newPassword" type="password" placeholder="Nova senha" />
+          <Input
+            name="ConfirmPassword"
+            type="password"
+            placeholder="Confirmação de senha"
+          />
+          <div>
+            <button type="submit">
+              <MdAddCircleOutline size={24} color="#fff" />
+              <strong>Salvar perfil</strong>
+            </button>
+          </div>
+        </Form>
+      </Content>
     </Container>
   )
 }
--- src/pages/Profile/sytles.js
+++ src/pages/Profile/sytles.js
@@ -1,3 +1,62 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  max-width: 940px;
+  margin: 0 auto;
+`
+export const Content = styled.div`
+  padding: 0 20px;
+  max-width: 940px;
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    input {
+      height: 50px;
+      max-width: 940px;
+      font-weight: bold;
+      background-color: #000000;
+      border: 0;
+      border-radius: 4px;
+      padding: 0 15px;
+      color: #ffffff;
+      margin: 0 0 20px;
+      opacity: 0.2;
+      font-size: 18px;
+      font-family: Helvetica;
+
+      &::placeholder {
+        color: rgba(255, 255, 255, 0.6);
+      }
+    }
+
+    hr {
+      margin-bottom: 20px;
+      height: 1px;
+      opacity: 0.1;
+      background-color: #ffffff;
+    }
+
+    div {
+      display: flex;
+      justify-content: flex-end;
+
+      button {
+        display: flex;
+        justify-content: flex-end;
+        border: 0;
+        border-radius: 6px;
+        background-color: #f94d6a;
+        color: #fff;
+        font-weight: bold;
+        padding: 14px 26px;
+
+        strong {
+          font-size: 22px;
+          margin-left: 10px;
+        }
+      }
+    }
+  }
+`
